30c4f1fc013d35b54ff7745b3e38cb15143f3f66
deepcoder007 <vibhanshuprasad@gmail.com>
2014-03-30 13:56:52 +0530

    added CHEFLUCK.cpp

---
+++ CHEFLUCK.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t,n,ans;
+	cin>>t;
+	int x;
+	while(t--)
+	{
+		cin>>n;
+		x=(int)(n/7);
+		while(x)
+		{
+			if( (n-7*x)%4==0)
+				 break;
+			x--;
+		}
+		if(x>0)cout<<x*7<<endl;
+		else cout<<-1<<endl;
+	}
+	return 0;
+}
+
+
