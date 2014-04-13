0414eedb7205ac235c330755c28e7e00f27931bc
deepcoder007 <vibhanshuprasad@gmail.com>
2014-04-13 16:29:41 +0530

    13 april 2014 , 4 o'clock

---
+++ STONES.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	string A;
+	string B;
+	int ans;
+	register int i;
+	int freq1[100];
+	int freq2[100];
+	while(t--)
+	{
+		cin>>A;
+		cin>>B;
+		ans=0;
+		//now measure thre freq
+		for(i=0;i<100;i++)
+		{ freq1[i]=freq2[i]=0; }
+		for(i=0;i<A.size();i++)
+			freq1[A[i]-'A']++;
+		for(i=0;i<B.size();i++)
+			freq2[B[i]-'A']++;
+		for(i=0;i<100;i++)
+		{
+			if( freq1[i]<=freq2[i] )
+				ans+=freq1[i];
+			else
+				ans+=freq2[i];
+		}
+		cout<<ans<<endl;
+	}
+	return 0;
+}
+
---
+++ tmp.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<'A'-'a'<<endl;
+	return 0;
+}
